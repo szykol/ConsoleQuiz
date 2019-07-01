918e05270cde944f8cda807a6c16517944b2e08b
szykol <szymonkolton@gmail.com>
2019-07-01 14:05:22 +0200

    Add QuizQuestion struct

---
+++ ConsoleQuiz/QuizQuestion.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project
+{
+    struct QuizQuestion
+    {
+        public string category;
+        public string type;
+        public string difficulty;
+        public string question;
+        public string correct_answer;
+        public string[] incorrect_answers;
+    }
+}
