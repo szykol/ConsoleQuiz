3eef794e1cbf40e809f2e39644249656475c959f
szykol <szymonkolton@gmail.com>
2019-07-01 14:23:12 +0200

    Display all questions and answers

--- ConsoleQuiz/Program.cs
+++ ConsoleQuiz/Program.cs
@@ -15,10 +15,32 @@ namespace Project
 
             var json = new WebClient().DownloadString("https://opentdb.com/api.php?amount=10&category=18");
             var m = JsonConvert.DeserializeObject<QuizResponse>(json);
-
+            var random = new Random();
             foreach (QuizQuestion question in m.results)
             {
                 Console.WriteLine(question.question);
+
+                var answers = new List<string>();
+                answers.AddRange(question.incorrect_answers);
+                answers.Insert(random.Next(0, answers.Count() + 1),question.correct_answer);
+                
+                var i = 0;
+                foreach(var answer in answers)
+                {
+                    Console.WriteLine($"{++i}. {answer}");
+                }
+
+                Console.Write("> ");
+                var index = int.Parse(Console.ReadLine());
+
+                if(answers[index - 1] == question.correct_answer)
+                {
+                    Console.WriteLine("Correct!");
+                }
+                else
+                {
+                    Console.WriteLine("Incorrect!");
+                }
             }
 
             // Bad Boys
