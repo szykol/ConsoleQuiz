a86d5b8150c6ba58faa1d4a796fa15efa1ff6ad0
szykol <szymonkolton@gmail.com>
2019-07-01 14:05:45 +0200

    Add QuizResponse struct

---
+++ ConsoleQuiz/QuizResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project
+{
+    struct QuizResponse
+    {
+        public int response_code;
+        public QuizQuestion[] results;
+    }
+}
