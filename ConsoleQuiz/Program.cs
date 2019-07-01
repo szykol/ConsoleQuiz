74c57ed160baf6624097a24eec764a16746ed351
szykol <szymonkolton@gmail.com>
2019-07-01 14:06:03 +0200

    Add Program.cs

---
+++ ConsoleQuiz/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Net;
+
+namespace Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var json = new WebClient().DownloadString("https://opentdb.com/api.php?amount=10&category=18");
+            var m = JsonConvert.DeserializeObject<QuizResponse>(json);
+
+            foreach (QuizQuestion question in m.results)
+            {
+                Console.WriteLine(question.question);
+            }
+
+            // Bad Boys
+            Console.ReadLine();
+        }
+    }
+}
