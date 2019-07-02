cd904e07c7c832718515da5e4a141ce82475a6c8
szykol <szymonkolton@gmail.com>
2019-07-02 12:23:56 +0200

    Move game's logic to separate class

--- ConsoleQuiz/Program.cs
+++ ConsoleQuiz/Program.cs
@@ -3,8 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Newtonsoft.Json;
-using System.Net;
 
 namespace ConsoleQuiz
 {
@@ -13,46 +11,9 @@ namespace ConsoleQuiz
         static void Main(string[] args)
         {
 
-            var json = new WebClient().DownloadString("https://opentdb.com/api.php?amount=10&category=18");
-            var m = JsonConvert.DeserializeObject<QuizResponse>(json);
-            var random = new Random();
-            var questionNumber = 1;
-            foreach (QuizQuestion question in m.results)
-            {
-                Console.WriteLine($"Question #{questionNumber++}: {question.question}");
+            var q = new Quiz();
 
-                var answers = new List<string>();
-                answers.AddRange(question.incorrect_answers);
-                answers.Insert(random.Next(0, answers.Count() + 1),question.correct_answer);
-                
-                var i = 0;
-                foreach(var answer in answers)
-                {
-                    Console.WriteLine($"{++i}. {answer}");
-                }
-
-                Console.Write("> ");
-                int index;
-                while (!int.TryParse(Console.ReadLine(), out index) || answers.ElementAtOrDefault(index - 1) == null)
-                {
-                    Console.WriteLine("Enter a correct number!");
-                    Console.Write("> ");
-                }
-
-                if (answers[index - 1] == question.correct_answer)
-                {
-                    Console.WriteLine("Correct!");
-                }
-                else
-                {
-                    Console.WriteLine("Incorrect!");
-                }
-                Console.ReadLine();
-                Console.Clear();
-            }
-
-            // Bad Boys
-            Console.ReadLine();
+            q.Start();
         }
     }
 }
---
+++ ConsoleQuiz/Quiz.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Net;
+
+namespace ConsoleQuiz
+{
+    class Quiz
+    {
+        QuizResponse quizReponse;
+        Random random = new Random();
+        int questionNumber = 1;
+
+        public void Start()
+        {
+            var url = "https://opentdb.com/api.php?amount=10&category=18";
+            var response = DownloadData(url);
+
+            foreach (QuizQuestion question in response.results)
+            {
+                Console.WriteLine($"Question #{questionNumber++}: {question.question}");
+
+                var answers = CreateResultsList(question);
+
+                var i = 0;
+                foreach (var answer in answers)
+                {
+                    Console.WriteLine($"{++i}. {answer}");
+                }
+
+                var index = FetchAnswerIndex(answers);
+
+                if (answers[index] == question.correct_answer)
+                    Console.WriteLine("Correct!");
+                else
+                    Console.WriteLine("Incorrect!");
+
+                Console.ReadLine();
+                Console.Clear();
+            }
+
+            Console.ReadLine();
+        }
+        QuizResponse DownloadData(string url)
+        {
+            var json = new WebClient().DownloadString(url);
+            var m = JsonConvert.DeserializeObject<QuizResponse>(json);
+
+            return m;
+        }
+        List<string> CreateResultsList(QuizQuestion q)
+        {
+            var answers = new List<string>();
+            answers.AddRange(q.incorrect_answers);
+            answers.Insert(random.Next(0, answers.Count() + 1), q.correct_answer);
+
+            return answers;
+        }
+
+        int FetchAnswerIndex(List<string> answers)
+        {
+            Console.Write("> ");
+            int index;
+            while (!int.TryParse(Console.ReadLine(), out index) || answers.ElementAtOrDefault(index - 1) == null)
+            {
+                Console.WriteLine("Enter a correct number!");
+                Console.Write("> ");
+            }
+
+            return index - 1;
+        }
+    }
+}
