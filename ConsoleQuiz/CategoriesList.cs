545848e6d72c4020f539a26d575bccafc2bcb48a
szykol <szymonkolton@gmail.com>
2019-07-02 13:41:39 +0200

    Add CategoriesList struct

---
+++ ConsoleQuiz/CategoriesList.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleQuiz
+{
+    struct CategoriesList
+    {
+        public struct Category
+        {
+            public string id;
+            public string name;
+        }
+
+        public List<Category> trivia_categories;
+    }
+}
