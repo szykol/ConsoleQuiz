e95213d4cb2b126675d840c819954205234ab41e
szykol <szymonkolton@gmail.com>
2019-07-03 11:53:02 +0200

    Add initial unit tests

---
+++ ConsoleQuiz.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ConsoleQuiz.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ConsoleQuiz.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c069148c-a66c-4f5c-b50f-421019451134")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ConsoleQuiz.UnitTests/QuizAPITests.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ConsoleQuiz.UnitTests
+{
+    [TestClass]
+    public class QuizAPITests
+    {
+        [TestMethod]
+        public void DefaultSettingsAfterInit()
+        {
+            QuizAPI api = new QuizAPI();
+
+            Assert.AreEqual(10, api.GetQuestionAmount());
+            Assert.AreEqual(0, api.GetCategory());
+            Assert.AreEqual(QuizAPI.Difficulty.ANY, api.GetDifficulty());
+        }
+
+        [TestMethod]
+        public void GetQuestions_WithDefaultSettings_ReturnsData()
+        {
+            QuizAPI api = new QuizAPI();
+
+            var questions = api.GetQuestions();
+            Assert.IsNotNull(questions);
+            Assert.IsTrue(questions.Length > 0);
+        }
+
+        [TestMethod]
+        public void SetCategory_BeforeFetchingCategories_ReturnsDefault()
+        {
+            QuizAPI api = new QuizAPI();
+
+            api.SetCategory(-1);
+            Assert.AreEqual(0, api.GetCategory());
+        }
+
+        [TestMethod]
+        public void SetCategory_WithIncorrectValue_ReturnsDefault()
+        {
+            QuizAPI api = new QuizAPI();
+
+
+            api.SetCategory(-1);
+            Assert.AreEqual(0, api.GetCategory());
+
+            api.SetCategory(80);
+            Assert.AreEqual(0, api.GetCategory());
+        }
+
+        
+    }
+}
