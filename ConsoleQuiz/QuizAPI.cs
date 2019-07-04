373935b36b5e44b34dcaa408600068de4047fe53
szykol <szymonkolton@gmail.com>
2019-07-04 09:50:52 +0200

    Add partial documentation for QuizAPI class

--- ConsoleQuiz/QuizAPI.cs
+++ ConsoleQuiz/QuizAPI.cs
@@ -19,6 +19,11 @@ namespace ConsoleQuiz
         int questionAmount = 10;
         Difficulty difficulty = Difficulty.ANY;
 
+        /// <summary>
+        /// Gets all categories from online API and returns them
+        /// in a list of strings
+        /// </summary>
+        /// <returns>List of Quiz Categories</returns>
         public List<string> GetCategoryList()
         {
             if (categoriesList == null)
@@ -30,6 +35,10 @@ namespace ConsoleQuiz
             return categoriesList.Select(category => category.name).ToList();
         }
 
+        /// <summary>
+        /// Sets the question category
+        /// </summary>
+        /// <param name="categoryListIndex">An index of category list returned from <c>GetCategoryList()</c></param>
         public void SetCategory(int categoryListIndex)
         {
             if (categoriesList != null && categoryListIndex > 0 && categoryListIndex <= categoriesList.Count)
@@ -38,16 +47,28 @@ namespace ConsoleQuiz
             }
         }
 
+        /// <summary>
+        /// Returns the category index
+        /// </summary>
+        /// <returns>Index of category list returned from <c>GetCategoryList()</c></returns>
         public int GetCategory()
         {
             return categoryIndex;
         }
 
+        /// <summary>
+        /// Returns the question amount
+        /// </summary>
+        /// <returns>Amount of question that will be downloaded from the API</returns>
         public int GetQuestionAmount()
         {
             return questionAmount;
         }
 
+        /// <summary>
+        /// Sets the question amount
+        /// </summary>
+        /// <param name="amount">Amount of question that will be downloaded from the API</param>
         public void SetQuestionAmount(int amount)
         {
             if (amount > 0)
@@ -56,16 +77,28 @@ namespace ConsoleQuiz
             }
         }
 
+        /// <summary>
+        /// Sets the question difficulty
+        /// </summary>
+        /// <param name="d">New difficulty</param>
         public void SetDifficulty(Difficulty d)
         {
             difficulty = d;
         }
 
+        /// <summary>
+        /// Returns the question difficulty
+        /// </summary>
+        /// <returns>The difficulty of question that will be downloaded from the API</returns>
         public Difficulty GetDifficulty()
         {
             return difficulty;
         }
 
+        /// <summary>
+        /// Exits the app with some string messages
+        /// </summary>
+        /// <param name="messages">Optional strings to describe the error</param>
         public static void ExitApp(params string[] messages)
         {
             Console.Clear();
@@ -79,17 +112,29 @@ namespace ConsoleQuiz
             Environment.Exit(-1);
         }
 
+        /// <summary>
+        /// Downloads and returns array of questions
+        /// </summary>
+        /// <returns>Array of questions</returns>
         public QuizQuestion[] GetQuestions()
         {
             questionList = DownloadQuestions();
             return questionList;
         }
 
+        /// <summary>
+        /// Returns QuestionManager and initializes it with question array
+        /// </summary>
+        /// <returns>QuestionManager that holds all questions and counts score</returns>
         public QuestionManager GetQuestionManager()
         {
             return new QuestionManager(DownloadQuestions());
         }
 
+        /// <summary>
+        /// Helper function that downloads categories from the API
+        /// </summary>
+        /// <returns>Categories list</returns>
         CategoriesList DownloadCategories()
         {
             var json = FetchJSON("https://opentdb.com/api_category.php");
@@ -99,6 +144,10 @@ namespace ConsoleQuiz
             return categories;
         }
 
+        /// <summary>
+        /// Helper function that downloads question from the API
+        /// </summary>
+        /// <returns>Array of questions</returns>
         QuizQuestion[] DownloadQuestions()
         {
             url = GenerateAPIUrl();
